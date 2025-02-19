c9907a9c510b26c7c7ca107ca0a25f674180a7b4
Bal1oon <ws32159@naver.com>
2025-02-19 15:39:40 +0900

    feat: 모든 영화 조회

--- src/main/java/cinebox/service/MovieService.java
+++ src/main/java/cinebox/service/MovieService.java
@@ -1,9 +1,14 @@
 package cinebox.service;
 
+import java.util.List;
+
 import cinebox.dto.request.MovieRequest;
 import cinebox.dto.response.MovieResponse;
 
 public interface MovieService {
 	// create
 	MovieResponse registerMovie(MovieRequest request);
+	
+	// read
+	List<MovieResponse> getAllMovies();
 }
--- src/main/java/cinebox/service/MovieServiceImpl.java
+++ src/main/java/cinebox/service/MovieServiceImpl.java
@@ -1,5 +1,8 @@
 package cinebox.service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Service;
 
 import cinebox.common.exception.movie.DuplicatedMovieException;
@@ -23,6 +26,12 @@ public class MovieServiceImpl implements MovieService {
 		Movie newMovie = Movie.register(request, null);
 		movieRepository.save(newMovie);
 		return MovieResponse.from(newMovie);
+	}
+
+	@Override
+	public List<MovieResponse> getAllMovies() {
+		List<Movie> movies = movieRepository.findAll();
+		return movies.stream().map(MovieResponse::from).collect(Collectors.toList());
 	}
 
 }
