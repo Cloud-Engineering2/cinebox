d35cd21fce556eb6531fce2b204c7ad21980fd7a
Bal1oon <ws32159@naver.com>
2025-02-21 02:00:34 +0900

    feat(#9): 유효성 검사 예외문 커스텀

--- src/main/java/cinebox/common/exception/ExceptionMessage.java
+++ src/main/java/cinebox/common/exception/ExceptionMessage.java
@@ -13,7 +13,10 @@ public enum ExceptionMessage {
 	
 	// movie
 	NOT_FOUND_MOVIE("유효하지 않은 MovieId 입니다.", HttpStatus.NOT_FOUND, "Not Found MovieId"),
-	DUPLICATED_MOVIE("이미 존재하는 영화입니다.", HttpStatus.CONFLICT, "Conflict Title and ReleaseDate");
+	DUPLICATED_MOVIE("이미 존재하는 영화입니다.", HttpStatus.CONFLICT, "Conflict Title and ReleaseDate"),
+	
+	// validation error
+	VALIDATION_ERROR("빈 값을 허용하지 않습니다.", HttpStatus.BAD_REQUEST, "Validation Error");
 	
 	private final String message;
 	private final HttpStatus status;
