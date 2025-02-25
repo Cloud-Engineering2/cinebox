ce622974137690401604e10b44d652923efacae4
SUJINJEONG012 <peekaboo32@naver.com>
2025-02-25 18:57:11 +0900

    Feat: 예매응답부분dto 확인

--- src/main/java/cinebox/dto/response/BookingResponse.java
+++ src/main/java/cinebox/dto/response/BookingResponse.java
@@ -8,21 +8,33 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class BookingResponse {
 
-	 	private Long bookingId;
-	    private LocalDateTime bookingDate;
-	    private Long screenId;
-	    private List<String> seatNumbers;
-	    private String status;
-	    private BigDecimal totalPrice;  // 여기서 totalPrice를 추가
-	    private String message;
-	    private String screenName;  // 상영관 이름 추가
+	private Long bookingId;
+	private LocalDateTime bookingDate;
+	private Long screenId;
+	private List<String> seatNumbers;
+	private String status;
+	private BigDecimal totalPrice; // 여기서 totalPrice를 추가
+	private String message;
+	private String screenName; // 상영관 이름 추가
 
+	public BookingResponse(Long bookingId, LocalDateTime bookingDate, Long screenId, List<String> seatNumbers,
+			String status, BigDecimal totalPrice, String screenName) {
+		this.bookingId = bookingId;
+		this.bookingDate = bookingDate;
+		this.screenId = screenId;
+		this.seatNumbers = seatNumbers;
+		this.status = status;
+		this.totalPrice = totalPrice;
+		this.screenName = screenName;
+	}
 
 }
