b6a292d88127ec06a42ddd0bdc88bda070190302
SUJINJEONG012 <peekaboo32@naver.com>
2025-02-25 18:58:29 +0900

    Feat:프론트단 컨트롤러 생성

---
+++ src/main/java/cinebox/controller/FrontController.java
@@ -0,0 +1,18 @@
+package cinebox.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FrontController {
+
+	
+		// 예매 
+		@GetMapping("/bookings/payment-test")
+		public String payment() {
+			return "booking/payment-test";
+		}
+	
+	
+	
+}
