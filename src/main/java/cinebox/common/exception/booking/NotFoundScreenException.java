036abcd53be4e13a984960453d339057d1b5c18c
SUJINJEONG012 <peekaboo32@naver.com>
2025-02-27 13:03:10 +0900

    Feat:예외처리메서드수정

--- src/main/java/cinebox/common/exception/booking/NotFoundScreenException.java
+++ src/main/java/cinebox/common/exception/booking/NotFoundScreenException.java
@@ -4,8 +4,10 @@ import cinebox.common.exception.BaseException;
 import cinebox.common.exception.ExceptionMessage;
 
 public class NotFoundScreenException extends BaseException {
-    public static final BaseException EXCEPTION = new NotFoundScreenException();
 
+	
+    public static final BaseException EXCEPTION = new NotFoundScreenException();
+    
     private NotFoundScreenException() {
         super(ExceptionMessage.NOT_FOUND_SCREEN);
     }
--- src/main/java/cinebox/common/exception/payment/NotFoundPaymentException.java
+++ src/main/java/cinebox/common/exception/payment/NotFoundPaymentException.java
@@ -1,5 +1,17 @@
 package cinebox.common.exception.payment;
 
-public class NotFoundPaymentException {
+import cinebox.common.exception.BaseException;
+import cinebox.common.exception.ExceptionMessage;
+
+public class NotFoundPaymentException  extends BaseException{
+
+
+	private static final long serialVersionUID = 1L;
+
+	public static final BaseException EXCEPTION = new NotFoundPaymentException();
+	private  NotFoundPaymentException() {
+		super(ExceptionMessage.NOT_FOUND_PAYMENT);
+		
+	}
 
 }
