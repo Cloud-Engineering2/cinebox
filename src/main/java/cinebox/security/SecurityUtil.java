e4dd9388197515ca64a60e056df7b7b87fa51538
Bal1oon <ws32159@naver.com>
2025-02-28 10:57:40 +0900

    feat(#53): 인증 유저 조회에 대한 공통 클래스 생성

---
+++ src/main/java/cinebox/security/SecurityUtil.java
@@ -0,0 +1,16 @@
+package cinebox.security;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import cinebox.entity.User;
+
+public class SecurityUtil {
+	public static User getCurrentUser() {
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		if (auth != null || auth.getPrincipal() instanceof PrincipalDetails) {
+			return ((PrincipalDetails) auth.getPrincipal()).getUser();
+		}
+		return null;
+	}
+}
