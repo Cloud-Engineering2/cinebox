c2c35cbf13c89227774761325b251805e25bd0bc
Bal1oon <ws32159@naver.com>
2025-03-05 14:38:00 +0900

    feat(#78): 사용자 조회 권한 추가

--- src/main/java/cinebox/controller/UserController.java
+++ src/main/java/cinebox/controller/UserController.java
@@ -3,6 +3,7 @@ package cinebox.controller;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,32 +22,34 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class UserController {
 	private final UserService userService;
-	
+
 	// 전체 사용자 조회
 	@GetMapping
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
 	public ResponseEntity<List<UserResponse>> getAllActiveUser() {
 		List<UserResponse> responses = userService.getAllActiveUser();
 		return ResponseEntity.ok(responses);
 	}
-	
+
 	// 사용자 정보 조회
 	@GetMapping("/{userId}")
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
 	public ResponseEntity<UserResponse> getUser(@PathVariable("userId") Long userId) {
 		UserResponse response = userService.getUserById(userId);
 		return ResponseEntity.ok(response);
 	}
-	
+
 	@PutMapping("/{userId}")
 	public ResponseEntity<UserResponse> updateUser(
 			@PathVariable("userId") Long userId,
-			@RequestBody UserUpdateRequest request) {    	
-	    UserResponse response = userService.updateUser(userId, request);
-    	return ResponseEntity.ok(response);	 
+			@RequestBody UserUpdateRequest request) {
+		UserResponse response = userService.updateUser(userId, request);
+		return ResponseEntity.ok(response);
 	}
-	
+
 	@DeleteMapping("/{userId}")
-	public ResponseEntity<String> deleteUser(@PathVariable("userId") Long userId) {		
+	public ResponseEntity<String> deleteUser(@PathVariable("userId") Long userId) {
 		userService.deleteUser(userId);
-		return ResponseEntity.ok().body("Success Delete");
+		return ResponseEntity.noContent().build();
 	}
 }
